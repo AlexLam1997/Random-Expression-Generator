151a1c2097c1dedaeb5d368eaf77e7b701442838
AlexLam1997 <x.ela24@hotmail.com>
2018-01-28 01:35:24 -0500

    Random Insult Generator

---
+++ app/src/main/java/com/example/ryan/insultgenerator/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.ryan.insultgenerator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void insult (View view){
+        View  = findViewById(R.id.button);
+
+    }
+}
